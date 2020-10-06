c3ab08db4f1978a939be6cb127c5ee3aee0ffe4f
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-06 15:43:16 +0100

    adding access the Yoputube API2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import youtube from "../api/youtube";
 import SearchBar from "./SearchBar";
 
 const accessKey = process.env.REACT_APP_YOUTUBE_KEY;
 
 class App extends React.Component {
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: "video",
+        key: accessKey,
+      },
+    });
+  };
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
