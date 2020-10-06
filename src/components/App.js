bb511669565e6bb6b14bdcdd765b42dc483ba03b
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-06 15:21:06 +0100

    adding access the Yoputube API

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
+const accessKey = process.env.REACT_APP_YOUTUBE_KEY;
+
 class App extends React.Component {
   render() {
     return (
